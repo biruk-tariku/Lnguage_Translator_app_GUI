7ceacff323b440a9e146f6f05bd760c4bf483b36
Biruk Tariku <128093428+biruk-tariku@users.noreply.github.com>
2023-09-18 12:18:19 +0300

    Update Language_Translator.py

--- Language_Translator.py
+++ Language_Translator.py
@@ -5,7 +5,6 @@ from googletrans import Translator, LANGUAGES
 root = Tk()
 root.geometry("1100x320")
 root.resizable(0,0)
-# root.iconbitmap('logo simpli.ico')
 root['bg']= 'black'
 
 root.title( 'Language translator by BURA')
