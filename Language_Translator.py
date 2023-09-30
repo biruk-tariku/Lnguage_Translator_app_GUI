a9cc9b79183b1b43b11ce0d19883351977e2d94c
Biruk Tariku <128093428+biruk-tariku@users.noreply.github.com>
2023-09-30 14:53:47 +0300

    Update Language_Translator.py

--- Language_Translator.py
+++ Language_Translator.py
@@ -34,7 +34,6 @@ def Translate():
 
 trans_btn = Button(root, text= 'Translate', font='arial 12 bold',pady= 5, command= Translate, bg="orange", activebackground="green")
 trans_btn.place(x=445, y=180 )
-
 root.mainloop()
 
  
